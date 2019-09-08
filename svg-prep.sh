397f70635f25680a9c3f5d94e28ab432df47251b
i-am-phi <phi@photon-platform.net>
2019-09-08 01:33:53 -0700

    styles integration

--- svg-prep.sh
+++ svg-prep.sh
@@ -1,7 +1,4 @@
-`#!/usr/bin/env bash
-
-# remove comments
-# remove clipPaths
+#!/usr/bin/env bash
 
 sed \
   -e '15a <style>' \
@@ -10,6 +7,8 @@ sed \
   -e '1,2d' \
   -e '/^$/d' \
   -e 's/<!--.*-->//' \
+  -e '/width=".*cm"/d' \
+  -e '/height=".*cm"/d' \
   -e '/<clipPath/,/<\/clipPath>/d' \
   -e 's/\s*clip-path="url(.*)"//' \
   -e 's/stroke-linejoin="round"\s*//' \
@@ -58,7 +57,7 @@ sed -i '
 
 # segments *******************************************************************
 sed  \
-  -e 's/stroke-width=".*" fill="none" stroke-opacity=".*" stroke="#\(.*\)"/class="segment \1"/' \
+  -e 's/stroke-width=".*" fill="none" stroke-opacity=".*" stroke="#\(.*\)"/class="segment s-\1"/' \
   harmonics/harmonics-07-pre3.svg > harmonics/harmonics-07-pre4.svg
 
 sed -i '
@@ -100,35 +99,55 @@ sed -i '
   ' \
   harmonics/harmonics-07-pre4.svg
 
-# lines & circles
+# lines ************************************************
 sed  \
-  -e 's/stroke-dasharray="1.0000,3.0000" fill="none" stroke="#\(.*\)"/class="line \1"/' \
-  -e 's/stroke-dasharray="5.0000,4.0000" fill="none" stroke="#\(.*\)"/class="circle \1"/' \
-  -e 's/ fill-opacity="1.0000" font-style="normal" font-family="Helvetica" font-weight="normal" stroke="none" fill="#ffcc66" font-size="12.000" x="0" y="0"//' \
+  -e 's/stroke-dasharray="1.0000,3.0000" fill="none" stroke="#\(.*\)"/class="line s-\1"/' \
   harmonics/harmonics-07-pre4.svg > harmonics/harmonics-07-pre5.svg
 
-sed -i '
-  # remove font glyphs
-  /<defs>/ , /<\/defs>/  d
+sed -i \
+-e '
+  # create id for lines
+  /<g>.*/ {
+    N
+    N
+    N
+    N
+    s/<g>.*\n\s*<g class="line\(.*\)">.*\n\(\s*<path.*>\n\)<title>Line \(.*\)<\/title>/<g id="i-\3">\n<g class="line\1">\n\2<title>Line \3<\/title>/
+  }
   ' \
-  harmonics/harmonics-07-pre5.svg > harmonics/harmonics-07-clean.svg
+  harmonics/harmonics-07-pre5.svg
 
+# circles
+sed  \
+  -e 's/stroke-dasharray="5.0000,4.0000" fill="none" stroke="#\(.*\)"/class="circle s-\1"/' \
+  harmonics/harmonics-07-pre5.svg > harmonics/harmonics-07-pre6.svg
 
-  # s/<g class="segment section">.*\n\(\s*<path.*>\n\)\(<title.*>\n\)\(<desc.*>\n\)</g>.*\n\(<g.*\n\)<text>\(.*\)<\/text>/<g class="segment section \5">\n\1\n\2\n\3<\g>\n\4\n<text>\5<\/text>/
+sed -i \
+  -e '
+    # create id for circles
+    /<g>.*/ {
+      N
+      N
+      N
+      N
+      s/<g>.*\n\s*<g class="circle\(.*\)">.*\n\(\s*<path.*>\n\)<title>Circle \(.*\)<\/title>/<g id="c-\3">\n<g class="circle\1">\n\2<title>Circle \3<\/title>/
+    }
+    ' \
+  harmonics/harmonics-07-pre6.svg
 
+sed  \
+  -e 's/fill-opacity=".*" fill-rule="evenodd" stroke="none" fill="#\(.*\)"/class="polygon s-\1"/' \
+  harmonics/harmonics-07-pre6.svg > harmonics/harmonics-07-clean.svg
 
-# sed '
-#   /<\/g>.*/ {
-#     N
-#     N
-# 	  #s/<\/g>.*\n<g>\n<g class="point top">/<g class="point top">/
-# 	  s/<\/g>/test/
-# 	}
-#   ' \
-#   > harmonics/harmonics-07-clean3.svg
-# sed \
-  # -e '/<\/g>/ , /<g class="point top">/  c "test" ' \
-
-  # -e 's/<\/g>\s*\n<g>\n<g class="point top">/<g class="point top">/' \
-  # -e 's/<\/g>\s*\n\<g>\n\<g class="point top">/<g class="point top">/' \
-`
+sed -i \
+  -e '
+    # create id for polygons
+    /<g>.*/ {
+      N
+      N
+      N
+      N
+      s/<g>.*\n\s*<g class="polygon\(.*\)">.*\n\(\s*<path.*>\n\)<title>\(.*\) \(.*\)<\/title>/<g id="y-\4">\n<g class="polygon\1">\n\2<title>\3 \4<\/title>/
+    }
+    ' \
+  harmonics/harmonics-07-clean.svg
